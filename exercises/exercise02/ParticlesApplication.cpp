dcdcced2ac76c4fca2526beb2a1c5b4058063d3b
nmbj <nimj@itu.dk>
2023-02-16 16:46:32 +0100

    exercise 02.2

--- exercises/exercise02/ParticlesApplication.cpp
+++ exercises/exercise02/ParticlesApplication.cpp
@@ -13,15 +13,16 @@ struct Particle
 {
     glm::vec2 position;
     // (todo) 02.X: Add more vertex attributes
+    float size;
  
 };
 
 // List of attributes of the particle. Must match the structure above
-const std::array<VertexAttribute, 1> s_vertexAttributes =
+const std::array<VertexAttribute, 2> s_vertexAttributes =
 {
     VertexAttribute(Data::Type::Float, 2), // position
     // (todo) 02.X: Add more vertex attributes
-
+    VertexAttribute(Data::Type::Float, 1)
 };
 
 
@@ -42,6 +43,7 @@ void ParticlesApplication::Initialize()
     m_mousePosition = GetMainWindow().GetMousePosition(true);
 
     // (todo) 02.2: Enable the GL_PROGRAM_POINT_SIZE feature on the device
+    GetDevice().EnableFeature(GL_PROGRAM_POINT_SIZE);
 
 
     // (todo) 02.3: Enable the GL_BLEND feature on the device
@@ -66,7 +68,7 @@ void ParticlesApplication::Update()
         // (todo) 02.X: Compute new particle attributes here
 
 
-        EmitParticle(mousePosition);
+        EmitParticle(mousePosition, Random01() * 20 + 5);
     }
 
     // save the mouse position (to compare next frame and obtain velocity)
@@ -142,13 +144,13 @@ void ParticlesApplication::InitializeShaders()
     }
 }
 
-void ParticlesApplication::EmitParticle(const glm::vec2& position)
+void ParticlesApplication::EmitParticle(const glm::vec2& position, const float size)
 {
     // Initialize the particle
     Particle particle;
     particle.position = position;
     // (todo) 02.X: Set the value for other attributes of the particle
-
+    particle.size = size;
 
     // Get the index in the circular buffer
     unsigned int particleIndex = m_particleCount % m_particleCapacity;
--- exercises/exercise02/shaders/particles.vert
+++ exercises/exercise02/shaders/particles.vert
@@ -1,6 +1,7 @@
 #version 330 core
 
 layout (location = 0) in vec2 ParticlePosition;
+layout (location = 1) in float ParticleSize;
 // (todo) 02.X: Add more vertex attributes
 
 
@@ -12,5 +13,6 @@ layout (location = 0) in vec2 ParticlePosition;
 
 void main()
 {
+	gl_PointSize = ParticleSize;
 	gl_Position = vec4(ParticlePosition, 0.0, 1.0);
 }
